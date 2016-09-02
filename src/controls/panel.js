db9fc4c8786e4cae80f04e736e2d732b7914d9e8
jcoupey <git@coupey.fr>
2016-09-02 08:38:25 +0200

    Add leaflet control for right panel. #2

---
+++ src/controls/panel.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var L = require('leaflet');
+
+var panelControl = L.Control.extend({
+  options: {
+    position: 'topright'
+  },
+
+  onAdd: function (map){
+    // Add reference to map.
+    map.panelControl = this;
+
+    // Main panel div.
+    this._div = L.DomUtil.create('div', 'panel-control');
+
+    // Header for panel control.
+    var headerDiv = document.createElement('div');
+    headerDiv.setAttribute('class', 'panel-header');
+    headerDiv.innerHTML = '<a href="http://vroom-project.org">VROOM</a>';
+    this._div.appendChild(headerDiv);
+
+    // Prevent events on this control to alter the underlying map.
+    if (!L.Browser.touch) {
+      L.DomEvent.disableClickPropagation(this._div);
+      L.DomEvent.on(this._div, 'mousewheel', L.DomEvent.stopPropagation);
+    }
+    else {
+      L.DomEvent.on(this._div, 'click', L.DomEvent.stopPropagation);
+    }
+
+    return this._div;
+  },
+
+  onRemove: function (map){
+    // Remove reference from map.
+    delete map.panelControl;
+  },
+
+  getWidth: function(){
+    return this._div.offsetWidth;
+  }
+});
+
+var panelControl = new panelControl();
+
+module.exports = panelControl;
--- src/map.js
+++ src/map.js
@@ -2,11 +2,13 @@
 
 var L = require('leaflet');
 var mapConfig = require('./config/leaflet');
+var panelControl = require('./controls/panel');
 
 // Define leaflet map.
 var map = L.map('map', {layers: [mapConfig.tileLayer]});
 map.setView(mapConfig.initCenter, mapConfig.initZoom);
 
+panelControl.addTo(map);
 module.exports = {
   map: map
 };
