5249d462ae4e8af8e5d54d8f48356e143150c51a
jcoupey <git@coupey.fr>
2016-09-09 08:19:15 +0200

    Improve control hide/display. #16

--- src/utils/locations.js
+++ src/utils/locations.js
@@ -9,10 +9,23 @@ var solveControl = require('../controls/solve');
 var panelControl = require('../controls/panel');
 var fitControl = require('../controls/fit');
 
-var checkSolveControl = function(){
+var checkControls = function(){
+  var hasJobs = (dataHandler.getJobsSize() > 0);
+  var hasStart = dataHandler.getStart();
+  var hasEnd = dataHandler.getEnd();
+  if(hasJobs || hasStart || hasEnd){
+    // Fit and clear controls as soon as we have a location.
+    if(!LSetup.map.fitControl){
+      LSetup.map.addControl(fitControl);
+    }
+    if(!LSetup.map.clearControl){
+      LSetup.map.addControl(clearControl);
+    }
+  }
   if(!LSetup.map.solveControl){
-    if((dataHandler.getStart() || dataHandler.getEnd())
-       && (dataHandler.getJobsSize() > 0)){
+    // Solve control appears only when there's enough input to fire a
+    // solving query.
+    if((hasStart || hasEnd) && hasJobs){
       solveControl.addTo(LSetup.map);
     }
   }
@@ -27,20 +40,15 @@ var checkSolveControl = function(){
 var addPlace = function(latlng, name){
   panelControl.hideInitDiv();
 
-  if(!LSetup.map.fitControl){
-    LSetup.map.addControl(fitControl);
-  }
-  if(!LSetup.map.clearControl){
-    LSetup.map.addControl(clearControl);
-  }
   if(dataHandler.isFirstPlace()){
     // Add vehicle start/end.
     dataHandler.firstPlaceSet();
 
     if(name){
       // Add start with provided name.
-      dataHandler.addStart(latlng, name, checkSolveControl);
-      dataHandler.addEnd(latlng, name, checkSolveControl);
+      dataHandler.addStart(latlng, name, checkControls);
+      dataHandler.addEnd(latlng, name, checkControls);
+      checkControls();
     }
     else{
       geocoder.defaultGeocoder.reverse(latlng, LSetup.map.options.crs.scale(19), function(results){
@@ -48,8 +56,9 @@ var addPlace = function(latlng, name){
         if(r){
           name = address.display(r);
           // Add start based on geocoding result.
-          dataHandler.addStart(latlng, name, checkSolveControl);
-          dataHandler.addEnd(latlng, name, checkSolveControl);
+          dataHandler.addStart(latlng, name, checkControls);
+          dataHandler.addEnd(latlng, name, checkControls);
+          checkControls();
         }
       });
     }
@@ -58,8 +67,8 @@ var addPlace = function(latlng, name){
     // Add regular job.
     if(name){
       // Add job with provided name.
-      dataHandler.addJob(latlng, name, checkSolveControl);
-      checkSolveControl();
+      dataHandler.addJob(latlng, name, checkControls);
+      checkControls();
     }
     else{
       geocoder.defaultGeocoder.reverse(latlng, LSetup.map.options.crs.scale(19), function(results){
@@ -67,8 +76,8 @@ var addPlace = function(latlng, name){
         if(r){
           name = address.display(r);
           // Add job based on geocoding result.
-          dataHandler.addJob(latlng, name, checkSolveControl);
-          checkSolveControl();
+          dataHandler.addJob(latlng, name, checkControls);
+          checkControls();
         }
       });
     }
