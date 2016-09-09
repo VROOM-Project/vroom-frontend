71cf3737c62d9f7e804fae0a650b25479a719b49
jcoupey <git@coupey.fr>
2016-09-09 08:55:25 +0200

    Update build to use local copies for dependecies css files. #1

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp node_modules/leaflet/dist/leaflet.css css/leaflet.css
+cp node_modules/leaflet-control-geocoder/dist/Control.Geocoder.css css/Control.Geocoder.css
+browserify -d src/map.js > bundle.raw.js && uglifyjs bundle.raw.js -c -m -o bundle.js
