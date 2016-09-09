cd362c57178512e77df59ca230a3d1b8f980f962
jcoupey <git@coupey.fr>
2016-09-09 17:14:45 +0200

    Host now defaults to the demo server. #5

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,9 @@
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
   description: '<a href="https://www.ens2m.fr/">ens2m</a>',
-  host: 'http://localhost',
-  port: '3000',
+  // Toogle the following to use a local vroom-server instance.
+  host: 'http://solver.vroom-project.org',
+  // host: 'http://localhost',
+  // port: '3000',
   maxJobNumber: 50
 };
