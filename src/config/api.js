4610fa62d5718780748caf0d499b7419e089d31e
jcoupey <git@coupey.fr>
2016-09-28 12:30:07 +0200

    Point to localhost.

--- src/config/api.js
+++ src/config/api.js
@@ -3,9 +3,7 @@
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
   description: '<a href="https://www.ens2m.fr/">ens2m</a>',
-  // Toogle the following to use a local vroom-server instance.
-  host: 'http://solver.vroom-project.org',
-  // host: 'http://localhost',
-  // port: '3000',
+  host: 'http://localhost',
+  port: '3000',
   maxJobNumber: 50
 };
