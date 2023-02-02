7f8c5dddeb64ac3443b66643e96c604bb08772e1
marym <marymcguire1718@gmail.com>
2023-02-02 17:02:46 +0100

    removing leftover overpass features

--- src/config/api.js
+++ src/config/api.js
@@ -4,6 +4,5 @@ module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
   host: 'https://routing.mastri-erp.de/route/',
   port: '',
-  maxTaskNumber: 100,
-  overpassEndpoint: 'https://overpass-api.de/api/interpreter'
+  maxTaskNumber: 100
 };
