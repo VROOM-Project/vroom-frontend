782133f3a7fb430c7b78b44a1379ab4611e58f6b
marym <marymcguire1718@gmail.com>
2023-02-02 16:32:37 +0100

    update vroom connection details

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: 'http://localhost',
-  port: '3000',
+  host: 'https://routing.mastri-erp.de/route/',
+  port: '',
   maxTaskNumber: 100,
   overpassEndpoint: 'https://overpass-api.de/api/interpreter'
 };
